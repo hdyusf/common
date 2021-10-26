2a7a35775dec6c2634ac3321922bef3669dbd258
黄钰森 <hsylm_hys@163.com>
2021-10-26 16:32:02 +0800

    feature: init

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import { parseUrl } from '../package/lib/index.js';
+let url = parseUrl(window.location)
+console.log('[ url ]', url);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
