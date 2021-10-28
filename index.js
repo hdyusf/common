9212543d3845c5b3f96ab03d7a6bdd62a2fb46c3
黄钰森 <hsylm_hys@163.com>
2021-10-28 18:21:28 +0800

    feature: del zip

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import array from './lib/array';
-import cookie from './lib/cookie';
-import country from './lib/country';
-import device from './lib/device';
-import dom from './lib/dom';
-import format from './lib/format';
-import fuc from './lib/function';
-import http from './lib/http';
-import judge from './lib/judge';
-import number from './lib/number';
-import object from './lib/object';
-import pxVw from './lib/pxVw';
-import scroll from './lib/scroll';
-import string from './lib/string';
-import url from './lib/url';
+import * as array from './lib/array';
+import * as cookie from './lib/cookie';
+import * as country from './lib/country';
+import * as device from './lib/device';
+import * as dom from './lib/dom';
+import * as format from './lib/format';
+import * as fuc from './lib/function';
+import * as http from './lib/http';
+import * as judge from './lib/judge';
+import * as number from './lib/number';
+import * as object from './lib/object';
+import * as pxVw from './lib/pxVw';
+import * as scroll from './lib/scroll';
+import * as string from './lib/string';
+import * as url from './lib/url';
 
 export default {
   ...array,
