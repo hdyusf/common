2e6eab141d4ae111c0a088b9dbabbb697fbf3c6a
黄钰森 <hsylm_hys@163.com>
2021-10-29 10:23:48 +0800

    feature: update index

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
-import * as array from './lib/array';
-import * as cookie from './lib/cookie';
-import * as country from './lib/country';
-import * as device from './lib/device';
-import * as dom from './lib/dom';
-import * as format from './lib/format';
-import * as fuc from './lib/function';
-import * as http from './lib/http';
-import * as judge from './lib/judge';
-import * as number from './lib/number';
-import * as object from './lib/object';
-import * as pxVw from './lib/pxVw';
-import * as scroll from './lib/scroll';
-import * as string from './lib/string';
-import * as url from './lib/url';
-
-export default {
-  ...array,
-  ...cookie,
-  ...country,
-  ...device,
-  ...dom,
-  ...format,
-  ...fuc,
-  ...http,
-  ...judge,
-  ...number,
-  ...object,
-  ...pxVw,
-  ...scroll,
-  ...string,
-  ...url,
+module.exports = {
+  ...require('./lib/array'),
+  ...require('./lib/cookie'),
+  ...require('./lib/country'),
+  ...require('./lib/device'),
+  ...require('./lib/dom'),
+  ...require('./lib/format'),
+  ...require('./lib/function'),
+  ...require('./lib/http'),
+  ...require('./lib/judge'),
+  ...require('./lib/number'),
+  ...require('./lib/object'),
+  ...require('./lib/pxVw'),
+  ...require('./lib/scroll'),
+  ...require('./lib/string'),
+  ...require('./lib/url'),
 };
